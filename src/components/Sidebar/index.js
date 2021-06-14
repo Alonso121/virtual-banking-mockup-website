c7ee5249b6791b0c633e0ec66e1eabdde4381351
Alonso121 <alison.tahiri@gmail.com>
2021-06-14 12:20:18 +0200

    Add Navbar with functionality and links

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SideBtnWrap,
+  SidebarRoute,
+} from "./SidebarElements";
+
+const navLinks = ["about", "discover", "services", "signup"];
+
+const Sidebar = ({ isOpen, toggle }) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          {navLinks.map((link) => (
+            <SidebarLink key={link} to={link} onClick={toggle}>
+              {link}
+            </SidebarLink>
+          ))}
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to="/signin">Sign In</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
