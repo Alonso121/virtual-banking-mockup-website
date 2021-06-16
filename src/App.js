37a127eb88038aa89233e246e115fd6bf9af5cfa
Alonso121 <alison.tahiri@gmail.com>
2021-06-16 13:21:04 +0200

    made Navbar available to Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 import Home from "./pages";
+import SigninPage from "./pages/signin";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <Router>
-      <Home />
+      <Navbar toggle={toggle} />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/signin" component={SigninPage} exact />
+      </Switch>
     </Router>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
-import React, { useState } from "react";
 import HeroSection from "../components/HeroSection";
 import InfoSection from "../components/InfoSection";
-import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
+
 import Services from "../components/Services";
 import {
   homeObjOne,
@@ -11,16 +9,8 @@ import {
 } from "../components/InfoSection/Data";
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <>
-      <Sidebar isOpen={isOpen} toggle={toggle} />
-      <Navbar toggle={toggle} />
       <HeroSection />
       <InfoSection {...homeObjOne} />
       <InfoSection {...homeObjTwo} />
